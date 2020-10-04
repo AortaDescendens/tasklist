4f8afc105608dce1a8e5dc632992e741c94cc965
AortaDescendens <33025401+AortaDescendens@users.noreply.github.com>
2020-10-05 02:49:38 +0300

    Second commit

--- tasklist.py
+++ tasklist.py
@@ -1,25 +1,67 @@
 # -*- coding: utf-8 -*-
 from PyQt5 import QtWidgets, QtCore, QtGui
+
 import sys
 import os
 import re
 import shutil
 import sqlite3
 
+from datetime import *
+from time import strftime, time, sleep
+
+import MainWindow
+
 #MainWindow
 #NewTask
 #TaskDetails
 #CompletedTasks
 
-class Aplication(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
+class Application(QtWidgets.QMainWindow, MainWindow.Ui_MainWindow):
 	def __init__(self):
 		super().__init__()
 		self.setupUi(self)
+		self.AddTask.clicked.connect(self.add_task)
+		self.ActualTasks.clicked.connect(self.view_actual)
+		self.CompletedTasks.clicked.connect(self.view_complited)
+		self.MainTable.itemDoubleClicked.connect(self.open_info)
+	
+	def add_task(self):
+		pass
+	
+	def view_actual(self):
+		pass
+	
+	def view_complited(self):
+		pass
+	
+	def open_info(self):
+		global id
 		
+		pass
+	
+	
+	
+class NewTask(QtWidgets.QMainWindow, NewTaskWindow.Ui_NewTaskWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+
+class TaskDetails(QtWidgets.QMainWindow, TaskDetailsWindow.Ui_TaskDetailsWindow):
+	def __init__(srlf):
+		super().__init__()
+		self.setupUi(self)
+
+
 
 def main():
+
 	global database
+	global today
+	
 	database = r'tasks.db'
+	today = datetime.now().strftime('%d.%m.%Y')
+	
 	app = QtWidgets.QApplication(sys.argv)
 	window = Application()
 	window.show()
